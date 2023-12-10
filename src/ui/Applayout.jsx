16d5f797d84e28f75ec53a83c9fc587ddbaff8c8
mohsen-a <mohsen.amanollahi98@gmail.com>
2023-12-10 19:35:20 +0330

    add header

--- src/ui/Applayout.jsx
+++ src/ui/Applayout.jsx
@@ -1,14 +1,20 @@
 import { Outlet } from "react-router-dom";
 import Header from "./Header";
 import Footer from "./Footer";
+import styled from "styled-components";
+
+const StyledAppLayout = styled.div`
+  max-width: 144rem;
+  margin: 0 auto;
+`;
 
 function Applayout() {
   return (
-    <div>
+    <StyledAppLayout>
       <Header />
       <Outlet />
       <Footer />
-    </div>
+    </StyledAppLayout>
   );
 }
 
--- src/ui/Header.jsx
+++ src/ui/Header.jsx
@@ -1,5 +1,28 @@
+import styled from "styled-components";
+import Logo from "./Logo";
+import HeaderIcons from "./HeaderIcons";
+import Sidebar from "./Sidebar";
+import Navbar from "./Navbar";
+
+const StyledHeader = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 1.6rem 2rem;
+  @media screen and (min-width: 1024px) {
+    padding: 3.2rem 10.8rem;
+  }
+`;
+
 function Header() {
-  return <div>header</div>;
+  return (
+    <StyledHeader>
+      <Sidebar />
+      <Logo />
+      <Navbar />
+      <HeaderIcons />
+    </StyledHeader>
+  );
 }
 
 export default Header;
